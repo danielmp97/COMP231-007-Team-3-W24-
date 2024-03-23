1d51c25a2b8d701475ce40541c35b344ae106ed8
rsotoacu <106275211+rsotoacu@users.noreply.github.com>
2024-03-23 01:08:20 -0400

    UI changes in colour palette and logo

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Homepage.css';
 import { jwtDecode } from "jwt-decode";
+import logo_slogan from '../assets/logo-slogan.png'; // Import your logo image file
+
 
 function Homepage() {
 
@@ -13,15 +15,30 @@ function Homepage() {
 
   return (
     <div>
-        <div className="homepage-container">  
-            <h1>Hello {user}</h1>
-            <h2>Welcome to Medical Appointment Scheduler</h2>
-            <h3>How it works?</h3>
-            <h3>This app allows you to easily schedule appointments with doctors.</h3>
-            <p>To start, click on the button "New Appointment" to create a new appointment with the doctor of your choice.
-                Fill out the form and you're all set!
-            </p>
-        </div>
+      <table id='home-message'>
+        <tr>
+          <td id='left-message'>
+            <div className="homepage-header">
+              <h1 id='welcome-text'>Hello!</h1>
+              <h1 id='user-name'>{user}</h1>
+              <h2 id='desctiption-text'>Welcome to Medical Appointment Scheduler</h2>
+            </div>
+          </td>
+          <td id='right-message'>
+            <div className="homepage-container">  
+              <h1 id='title-container'>Your next appointment:</h1>
+              <hr></hr>
+            </div>
+          </td>
+        </tr>
+      </table>
+      {/* <h2>Welcome to Medical Appointment Scheduler</h2>
+              <h3>How it works?</h3>
+              <h3>This app allows you to easily schedule appointments with doctors.</h3>
+              <p>To start, click on the button "New Appointment" to create a new appointment with the doctor of your choice.
+                  Fill out the form and you're all set!
+              </p> */}
+
     </div>
   );
 }
--- client/src/pages/NavigationBar.jsx
+++ client/src/pages/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom'; 
 import './NavigationBar.css';
+import logo from '../assets/logo.png'; // Import your logo image file
 
 function NavigationBar() {
   const location = useLocation();
@@ -11,14 +12,17 @@ function NavigationBar() {
   }
 
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/my-appointments">My Appointments</Link></li>
-        <li><Link to="/create-appointment">New Appointment</Link></li>
-        <li><Link to="/my-profile">My Profile</Link></li>
-        <li><button onClick={handleSignOut} className='signout-button'>Sign Out</button></li>
-      </ul>
+    <nav className="nav">
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="nav-options">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/my-appointments">My Appointments</Link></li>
+          <li><Link to="/create-appointment">New Appointment</Link></li>
+          <li><Link to="/my-profile">My Profile</Link></li>
+        </ul>
+      </div>
+      <button onClick={handleSignOut} className='signout-button'>Sign Out</button>
     </nav>
   );
 }
