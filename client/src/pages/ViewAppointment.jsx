04fb53efa2e8a5717571939a9cb87a444383f20f
Daniel Mayorga <daniel.mayorga97@hotmail.com>
2024-04-03 18:35:31 -0400

    fixed bug when trying to see the details of an appointment with a deleted patient user

--- client/src/pages/ViewAppointment.jsx
+++ client/src/pages/ViewAppointment.jsx
@@ -1,82 +1,87 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import  './ViewAppointment.css'
+import './ViewAppointment.css';
 
 const ViewAppointment = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [appintmentData,setAppointmentData]=useState()
-  const [patient,setPatient]=useState()
+  const [searchParams] = useSearchParams();
+  const [appointmentData, setAppointmentData] = useState(null);
+  const [patient, setPatient] = useState(null);
+  const [patientExist, setPatientExist] = useState(true);
   const [renderApp, setRenderApp] = useState(false);
   const appointmentId = searchParams.get("id");
 
-
-  console.log(appointmentId);
   const loadData = async () => {
     try {
       const response = await axios.get(
         `http://localhost:8000/appointments/${appointmentId}`
       );
-      console.log(response);
 
       if (response.status === 200) {
-       setAppointmentData(response?.data);
+        setAppointmentData(response?.data);
         const patientData = await axios.get(
           `http://localhost:8000/patients/${response?.data?.patient}`
         );
 
-        if (response.status === 200) {
+        if (patientData.status === 200) {
+          setPatient(patientData.data);
+          setPatientExist(true);
+          setRenderApp(true);
         }
-        setPatient(patientData.data);
-        setRenderApp(true);
       }
     } catch (err) {
       console.log(err);
+      setPatientExist(false); 
+      setRenderApp(true); 
     }
   };
 
   useEffect(() => {
     loadData();
-  }, []);
+  }, []); 
+
   return (
     <>
       {renderApp ? (
         <div className="viewContainer container">
-            {console.log(patient)}
-          <div className={`heading`}>Appointment Details</div>
+          {patientExist ? (
+            <>
+              <div className={`heading`}>Appointment Details</div>
+              <ul className={`appointmentData`}>
+                <li>Appointment Date: {new Date(appointmentData.dateTime).toLocaleDateString()}</li>
+                <li>Appointment Time:
+                  {new Date(appointmentData.dateTime).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </li>
+                <li>Reason: {appointmentData.reason}</li>
+                <li>Notes: {appointmentData.notes}</li>
+                <li>Doctor Name: {appointmentData.doctorName}</li>
+              </ul>
 
-          <ul className={`appointmentData`}>
-            <li>Appointment Date: {new Date(appintmentData.dateTime).toLocaleDateString()}</li>
-            <li>Appointment Time:
-              {new Date(appintmentData.dateTime).toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </li>
-            <li>Reason: {appintmentData.reason}</li>
-            <li>Notes: {appintmentData.notes}</li>
-            <li>Doctor Name: {appintmentData.doctorName}</li>
-          </ul>
-
-          <div className={`heading`}>Patient Details</div>
-          <ul className={``}>
-            <li>Patient Name: {patient.name}</li>
-            <li>Email: {patient.email}</li>
-            <li>Phone: {patient.phone}</li>
-            <li>Gender: {patient.gender}</li>
-            <li>Age: {patient.age}</li>
-            <li>Notes: {patient.notes}</li>
-           
-          </ul>
-          
+              <div className={`heading`}>Patient Details</div>
+              <ul className={``}>
+                <li>Patient Name: {patient.name}</li>
+                <li>Email: {patient.email}</li>
+                <li>Phone: {patient.phone}</li>
+                <li>Gender: {patient.gender}</li>
+                <li>Age: {patient.age}</li>
+                <li>Notes: {patient.notes}</li>
+              </ul>
+            </>
+          ) : (
+            <div>
+              <h2>Patient Data Not Found</h2>
+              <h2>Patient may have been deleted</h2>  
+            </div>
+          )}
         </div>
-      ):(
-
+      ) : (
         <div className="container">
-            <div className="heading">Loading ...</div>
+          <div className="heading">Loading ...</div>
         </div>
-      )
-      }
+      )}
     </>
   );
 };
