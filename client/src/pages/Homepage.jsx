9928b2de18dbe5ee6f8e23791389b35cb78a98c2
rsotoacu <106275211+rsotoacu@users.noreply.github.com>
2024-04-04 17:45:56 -0400

    Home page buttons working

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Homepage.css';
 import { jwtDecode } from "jwt-decode";
+import { useNavigate } from 'react-router-dom'; 
 
 function Homepage() {
 
@@ -19,60 +20,71 @@ function Homepage() {
   const user = jwtDecode(getToken()).name;
 
   const userId = jwtDecode(getToken()).userId;
-    console.log(userId);
+  console.log(userId);
 
-    useEffect(() => {
-      const userType = jwtDecode(getToken()).role;
-      setUserType(userType); // Set the userType state
-    
-      if (userType === 'doctor') {
-        setIsDoctor(true);
-      } else if (userType === 'patient') {
-        setIsPatient(true);
-      } else if (userType === 'front desk' || userType === 'IT staff') {
-        setIsStaff(true);
-      }
-    
-      const getNextAppointments = async () => {
-        try {
-          const response = await fetch(URL + 'appointments');
-          const data = await response.json();
-          console.log(data);
-          
-          if (data.length > 0) {
-            // Filter appointments for the current day
-            const currentDate = new Date().toLocaleDateString();
-            console.log(currentDate);
-            let filteredAppointments = [];
-    
-            if (isDoctor) {
-              // Filter appointments for the current doctor
-              filteredAppointments = data.filter(appointment => appointment.doctor === userId);
-            } else if (isPatient) {
-              // Filter appointments for the current patient
-              filteredAppointments = data.filter(appointment => appointment.patient === userId);
-            } else if (isStaff) {
-              // Filter appointments for all users
-              filteredAppointments = data;
-            }
-    
-            // Filter appointments for the current day
-            filteredAppointments = filteredAppointments.filter(appointment => new Date(appointment.dateTime).toLocaleDateString() === currentDate);
-    
-            // Sort appointments by time
-            const sortedAppointments = filteredAppointments.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
-            
-            // Set the next appointments
-            setNextAppointments(sortedAppointments);
+  useEffect(() => {
+    const userType = jwtDecode(getToken()).role;
+    setUserType(userType); // Set the userType state
+
+    if (userType === 'doctor') {
+      setIsDoctor(true);
+    } else if (userType === 'patient') {
+      setIsPatient(true);
+    } else if (userType === 'front desk' || userType === 'IT staff') {
+      setIsStaff(true);
+    }
+
+    const getNextAppointments = async () => {
+      try {
+        const response = await fetch(URL + 'appointments');
+        const data = await response.json();
+        console.log(data);
+        
+        if (data.length > 0) {
+          // Filter appointments for the current day
+          const currentDate = new Date().toLocaleDateString();
+          console.log(currentDate);
+          let filteredAppointments = [];
+
+          if (isDoctor) {
+            // Filter appointments for the current doctor
+            filteredAppointments = data.filter(appointment => appointment.doctor === userId);
+          } else if (isPatient) {
+            // Filter appointments for the current patient
+            filteredAppointments = data.filter(appointment => appointment.patient === userId);
+          } else if (isStaff) {
+            // Filter appointments for all users
+            filteredAppointments = data;
           }
-        } catch (error) {
-          console.error('Error:', error);
+
+          // Filter appointments for the current day
+          filteredAppointments = filteredAppointments.filter(appointment => new Date(appointment.dateTime).toLocaleDateString() === currentDate);
+
+          // Sort appointments by time
+          const sortedAppointments = filteredAppointments.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
+          
+          // Set the next appointments
+          setNextAppointments(sortedAppointments);
         }
-      };
-    
-      getNextAppointments();
-    }, [isDoctor, isPatient, isStaff]); 
-    
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    getNextAppointments();
+  }, [isDoctor, isPatient, isStaff]); 
+
+  const navigate = useNavigate(); // Define the navigate function
+
+  const handleNewAppointment = () => {
+    // Redirect the user to the new Appointment page after signing out
+    navigate('/create-appointment');
+  };
+
+  const handleAllAppointments = () => {
+    // Redirect the user to the all Appointments page after signing out
+    navigate('/my-appointments');
+  };
 
   return (
     <div>
@@ -84,7 +96,7 @@ function Homepage() {
               <h1 id='welcome-text'>Hello!</h1>
               <h1 id='user-name'>{user}</h1>
               <h2 id='desctiption-text'>Welcome to MedLink, schedule your health today!</h2>
-              <h1 id='user-name'>{userType}</h1>
+              <h1 id='desctiption-text'>You are logged in as: {userType}</h1>
             </div>
           </td>
           <td id='right-message'>
@@ -143,9 +155,9 @@ function Homepage() {
         </tr>
         <tr>
           <td colSpan="2" id='button-section'>
-            <button id='btn-cancel'>Cancel Appointment</button>
-            <button id='btn-new'>Book a New Appointment</button>
-            <button id='btn-viewAll'>View All My Appointments</button>
+            {/* <button id='btn-cancel'>Cancel Appointment</button> */}
+            <button onClick={handleNewAppointment} id='btn-new'>Book a New Appointment</button>
+            <button onClick={handleAllAppointments} id='btn-viewAll'>View All My Appointments</button>
           </td>
         </tr>
       </tbody>
